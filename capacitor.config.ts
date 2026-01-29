afa04edecc524413277659f62e5379b80c425ce8
Simrankumari1603 <simarn2936kumari@gmail.com>
2026-01-29 23:28:41 +0530

    Fee For Schhol APK

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.feemanager.app',
+  appName: 'Fee Manager',
+  webDir: 'dist'
+};
+
+export default config;
